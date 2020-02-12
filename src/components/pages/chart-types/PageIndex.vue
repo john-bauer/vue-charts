19686430436626844f86d34a48f7d8f14e670c42
John Bauer <johnbauer126@gmail.com>
2020-02-11 21:19:01 -0500

    add chart types page

---
+++ src/components/pages/chart-types/PageIndex.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="-chart-types-page-index">
+    chart types page
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChartTypesPageIndex"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
---
+++ src/views/ChartTypes.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="chart-types-page">
+    <ChartTypesPageIndex />
+  </div>
+</template>
+
+<script>
+import ChartTypesPageIndex from "@/components/pages/chart-types/PageIndex.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ChartTypesPageIndex
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
